54d3455d00b8daa088dda8c96363fdfc96dd42c5
Primi <angel-primi@hotmail.com>
2024-02-27 11:25:13 -0600

    Send example

---
+++ examples/2_SEND/2_SEND.ino
@@ -0,0 +1,85 @@
+#include <Wire.h>             // I2c
+#include <Adafruit_GFX.h>     // OLED
+#include <Adafruit_SSD1306.h> // OLED
+
+#include <SPI.h>  // SPI
+#include <LoRa.h> // LoRa
+
+// define the pins used by the LoRa transceiver module
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+
+// 433E6 for Asia
+// 866E6 for Europe
+// 915E6 for North America
+#define BAND 915E6
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup()
+{
+    // initialize serial
+    Serial.begin(115200);
+
+    // Check if the OLED is connected on Address 0x3C
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    {
+        Serial.println(F("SSD1306 allocation failed"));
+        while (1)
+            ; // Don't proceed, loop forever
+    }
+
+    display.clearDisplay();      // Clear the screen
+    display.setTextSize(1);      // Set text size
+    display.setTextColor(WHITE); // Set text color
+    display.setCursor(0, 0);
+    display.println("LoRa sender"); // Display static text
+    display.display();
+    delay(1000);
+
+    // SPI LoRa pins
+    SPI.begin(SCK, MISO, MOSI, SS);
+    LoRa.setPins(SS, RST, DIO0); // setup LoRa transceiver module
+
+    // initialize LoRa transceiver module
+    if (!LoRa.begin(BAND))
+    {
+        display.println("LoRa init failed.");
+        display.display();
+        while (1)
+            ;
+    }
+    display.println("LoRa init succeeded.");
+    display.display();
+    delay(1000);
+}
+
+void loop()
+{
+    byte i = 0;
+    while (1)
+    {
+        // Message
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.print("Sending: ");
+        display.println(i);
+        display.display();
+
+        // send packet
+        LoRa.beginPacket();
+        LoRa.print(i); // sends number as string, for raw bytes use LoRa.write()
+        LoRa.write(i); // sends number as raw bytes
+        LoRa.endPacket();
+        delay(400);
+        i++;
+    }
+}
